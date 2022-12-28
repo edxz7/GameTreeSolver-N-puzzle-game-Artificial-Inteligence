878905309b6952cac84856d298c193feb01819c0
Eduardo Ch Colorado <eduardo.chavez@kavak.com>
2022-12-27 21:12:43 -0600

    chore: update code with type hints

--- board.py
+++ board.py
@@ -1,61 +1,82 @@
 import random
+from typing import Final
 
-#*****************************************************************************
-#  Execution:    none 
-#  Dependencies: none
+# *****************************************************************************
+#  Execution:    none
+#  Dependencies: nonez
 #  Representation of a n-puzzle board object
 #  @author Eduardo Ch. Colorado
-#******************************************************************************/
+# ******************************************************************************/
+
 
 class Board(object):
+    """
+    Defines a Board for the n-puzzle game
+    """
+
     # Constructor
-    def __init__(self,blocks):
-        self.n = len(blocks)
-        self.board = []        
-        self.Manhattan = 0
-        self.Hamming = 0
+    def __init__(self, blocks: list[list[int]]):
+        """
+        Creates an instance of a board
+        Internally, the content of the board is represented as 1D array
+        where each row in the 2D board is append to the right, from top
+        to bottom
+        Also the Manhattan and Hamming are calculated as private properties of the class
+        """
+        self.n: Final[int] = len(blocks)
+        self.linear_board: list[int] = []
+        self.Manhattan: int = 0
+        self.Hamming: int = 0
         for i in range(self.n):
             for j in range(self.n):
                 entry = blocks[i][j]
-                self.board.append(entry) 
-                if (entry!=0):
-                    self.Manhattan += abs(i-(entry-1)//self.n) + abs(j-((entry-1)%self.n))
-                    if (entry != self.n*i + j + 1): self.Hamming+=1
+                self.linear_board.append(entry)
+                if entry != 0:
+                    self.Manhattan += abs(i - (entry - 1) // self.n) + abs(
+                        j - ((entry - 1) % self.n)
+                    )
+                    if entry != self.n * i + j + 1:
+                        self.Hamming += 1
 
-    
     def dimension(self):
-        '''Size of the board game'''
+        """Size of the board game"""
         return self.n
 
     def manhattan(self):
-        '''Manhattan distance'''
+        """Manhattan distance"""
         return self.Manhattan
+
     def hamming(self):
-        '''Hamming distance'''
+        """Hamming distance"""
         return self.Hamming
 
     def inversions(self):
         pass
-    def is_goal(self):
-        '''Determine when a given board is the goal board'''
-        for i in range(0,self.n*self.n - 1):
-            if(self.board[i] != i+1): return False
+
+    def is_goal(self) -> bool:
+        """Determine when a given board is the goal board"""
+        for i in range(0, self.n * self.n - 1):
+            if self.linear_board[i] != i + 1:
+                return False  # if the linear array is not sorted in ascending order, then the board is not the goal board
         return True
 
     def __get_board(self):
-        return self.board
-    
-    def __swap(self,Idx,nIdx):
-        '''Swap one valid place the blank tile'''
-        neighbour = [[self.board[i*self.n + j] for j in range(self.n)] for i in range(self.n)]
-        i, j =  Idx // self.n , Idx % self.n
-        l, m = nIdx // self.n,  nIdx % self.n
-        temp = self.board[nIdx]
-        neighbour[i][j] = temp
-        neighbour[l][m] = 0
-        return neighbour
+        return self.linear_board
 
-    ##Overload the comparisson methods
+    def __swap(self, Idx: int, nIdx: int):
+        """Swap one valid place in the blank tile"""
+        neighbor = [
+            [self.linear_board[i * self.n + j] for j in range(self.n)]
+            for i in range(self.n)
+        ]
+        i, j = Idx // self.n, Idx % self.n
+        l, m = nIdx // self.n, nIdx % self.n
+        temp = self.linear_board[nIdx]
+        neighbor[i][j] = temp
+        neighbor[l][m] = 0
+        return neighbor
+
+    ##Overload the comparison method
     def __eq__(self, other):
         if isinstance(other, self.__class__):
             return self.__dict__ == other.__dict__
@@ -66,34 +87,46 @@ class Board(object):
         # if(not isinstance(other, Board)): return False
         # other_board = other.__get_board()
         # for i in range(self.n*self.n):
-        #     if(self.board[i] != other_board[i]): return False
+        #     if(self.linear_board[i] != other_board[i]): return False
         # return True
 
     # Iterate through the possibles bottom, up, left and right blank tiles moves (we called them neighbors)
     def neighbors(self):
-        '''Return the neighbors of a given board'''
-        #search where the blank space is placed
-        idx = self.board.index(0)
-        #create a neighboard list
-        L = []
-        if(idx // self.n != 0):                                 L.append(Board(self.__swap(idx, idx - self.n)))  #up neighbour
-        if((idx + 1)//self.n == idx//self.n):                   L.append(Board(self.__swap(idx, idx + 1 )))      #right neighbour
-        if(idx + self.n < self.n**2):                           L.append(Board(self.__swap(idx, idx + self.n)))  #botton neighopur
-        if((idx - 1)//self.n == idx//self.n and idx - 1 >= 0):  L.append(Board(self.__swap(idx, idx - 1)))       #left neighbour
+        """
+        Return the possible boards after moving the blank tile to valid positions
+        with respect to the reference Board
+        """
+        # search where the blank space is placed
+        idx = self.linear_board.index(0)
+        # create a neighbor list
+        L: list[Board] = []
+        if idx // self.n != 0:
+            L.append(Board(self.__swap(idx, idx - self.n)))  # up neighbor
+        if (idx + 1) // self.n == idx // self.n:
+            L.append(Board(self.__swap(idx, idx + 1)))  # right neighbor
+        if idx + self.n < self.n**2:
+            L.append(Board(self.__swap(idx, idx + self.n)))  # botton neighopur
+        if (idx - 1) // self.n == idx // self.n and idx - 1 >= 0:
+            L.append(Board(self.__swap(idx, idx - 1)))  # left neighbor
 
-        while(len(L) != 0):   
+        while len(L) != 0:
             yield L.pop()
 
-    def __str__(self):
-        s = "\n".join('  '.join([str(self.board[i*self.n + j]) for j in range(self.n)]) for i in range(self.n))
+    def __str__(self) -> str:
+        s = "\n".join(
+            "  ".join([str(self.linear_board[i * self.n + j]) for j in range(self.n)])
+            for i in range(self.n)
+        )
         return s + "\n"
+
+
 # n = 3
 # a = list(random.sample(range(n*n),n*n))
 
 # blocks1 = [[a[n*i+j] for j in range(n)] for i in range(n) ]
 
 # blocks2 = [[a[n*i+j] for j in range(n)] for i in range(n) ]
-# temp = blocks2[1][2] 
+# temp = blocks2[1][2]
 # blocks2[1][2] = blocks2[2][1]
 # blocks2[2][1] = temp
 # s = "\n".join(' '.join([str(blocks[i][j]) for j in range(3)]) for i in range(3))
@@ -110,11 +143,8 @@ class Board(object):
 # print()
 # print(board1.is_goal())
 # print()
-# for neighbour in board1.neighbors():
-#     print(neighbour)
-#     print('Manhattan distannce: ' + str(neighbour.manhattan()))
-#     print('Hamming distance: ' + str(neighbour.hamming()))
+# for neighbor in board1.neighbors():
+#     print(neighbor)
+#     print('Manhattan distannce: ' + str(neighbor.manhattan()))
+#     print('Hamming distance: ' + str(neighbor.hamming()))
 #     print()
-
-
-
--- solver.py
+++ solver.py
@@ -1,224 +1,276 @@
 import minPQ
-from board import Board # from the file import the class name
+from board import Board  # from the file import the class name
 import random
 from heapq import heappop, heappush
 from pathlib import Path
+from enum import Enum, auto
 
-#*****************************************************************************
-#  Execution:    python solver.py 
+# *****************************************************************************
+#  Execution:    python solver.py
 #  Dependencies: none
 #
 #  Game tree solver. The initial board is used as the root of a game tree. The child
 #  nodes represents possible configurations of the initial board.
 #  Using a min priority queue and the A* algorithm, this code finds the solution of
-#  the the n-puzzle game with the minimum number of moves and prints the steps you 
-#  need to follow to reach the soltion in the real life.
+#  the n-puzzle game with the minimum number of moves and prints the steps you
+#  need to follow to reach the solution in the real life.
 #
 #  You can build your own board of size n x n using the following example:
-#  
+#
 #  3
 #  9  7  3
 #  4  5  6
 #  1  2  0
 #
-#  The 0 reprecents the black tile in the n-puzzle game
+#  The 0 represents the black tile in the n-puzzle game
 #
 #  @author Eduardo Ch. Colorado
-#******************************************************************************/
+# ******************************************************************************/
 
 
 # #############################################################################/
 # How the algorithm works?
-# To solve any given game board, a game tree is build with the initial 
+# To solve any given game board, a game tree is build with the initial
 # board as the root of the tree
 
-# Breafly the solution is based on:
+# Briefly, the solution is based on:
 
 # A* algorithm -> to minimize f (cost function) where f = g + h
 # g = number of moves (goal distance)
 # h = manhattan or hamming distance (heuristic distance)
 
-# In this code I used the word "neighbors" to named the different possible configurations  
-# for a board when you swap the blank tile one place to the left, rigth, up or bottom. 
-# For example :
-    
+# In this code I used the word "neighbors" to named the different possible configurations
+# for a board when you swap the blank tile one place to the left, right, up or bottom.
+# For example:
+
 # Initial board                                       "neighbors"
 #    9  7  4                  9  0  4          9  7  4          9  7  4         9  7  4
 #    1  0  3         ->       1  7  3          0  1  3          1  3  0         1  6  3
 #    2  6  5                  2  6  5          2  6  5          2  6  5         2  0  5
 
 
-# For further ilustration, we called n1, n2 and n3 to the "neighbours" of the initial board 
+# For further illustration, we called n1, n2 and n3 to the "neighbors" of the initial board
 # and we assume n2 has the lowest h value in the following diagram:
 #
 #                                number of
-#                                iterations             Game Tree
+#                                iterations          Game Tree
 # _______________________________________________________________
 #     MinPQ                          0             initial board
 #     -----                                              |
 #     -----                                          /   |    \
 #   n1-----                                         /    |     \
-#   n3-----   node with              1             n1    n2    n3         three different 
+#   n3-----   node with              1             n1    n2    n3         three different
 #   n2----- <- lowest                                    ^                configurations
 #              f cost                                    |
 #                                    2          n1, n2, and n3 are added
-#                                    .          into the minPQ but n2 will 
+#                                    .          into the minPQ but n2 will
 # The MinPQ always has the           .          be pop in the next step,
-# board with lower f cost            .          then its neightbours will be
-# ready to be dequeued on            .          added again into the minPQ 
-# each iteration. Other              .          and ordered by its f cost 
+# board with lower f cost            .          then its neighbors will be
+# ready to be dequeued on            .          added again into the minPQ
+# each iteration. Other              .          and ordered by its f cost
 # added boards with slightly                    taking into account n1 and n3.
-# higher f cost are "waiting"                   This process is repeated until 
-# its turn to be dequeued            .          the goal is reached (if it exist)  
-#                                    .           
-#    
-#                                   
-# the number of iterations increases on each level of the game tree. The number of   
+# higher f cost are "waiting"                   This process is repeated until
+# its turn to be dequeued            .          the goal is reached (if it exist)
+#                                    .
+#
+#
+# the number of iterations increases on each level of the game tree. The number of
 # moves is recorded internally in the node object in this implementation
 ##############################################################################/z
 
+
+class BoardNode(object):
+    """
+    Recursive data structure, a wrapper for the class board to make a linked list
+    current: (Board) the current board instance in the solver
+    state:   (int) the current state (move) in the solver
+    link:    (BoardNode) a link to the precedent BoardNode object
+    """
+
+    def __init__(self, current: Board, state: int, link: Board):
+        self.current = current
+        self.state = state
+        self.link = link
+
+
+class HeuristicDistance(Enum):
+    """
+    Available heuristic distances to solve the 8-puzzle game
+    """
+
+    MANHATTAN = auto()
+    HAMMING = auto()
+
+
 class Solver(object):
-    '''
-    A class created to solve the 8-puzzle using a min priority queue and the 
-    A* algorithm. It take a two dimensional array as a representaion of a 
-    valid game board an search the minimum number of moves to solve the puzzle 
-    optimizing the following cost funtion:
+    """
+    A class created to solve the 8-puzzle using a min priority queue and the
+    A* algorithm. It take a two dimensional array as a representation of a
+    valid game board an search the minimum number of moves to solve the puzzle
+    optimizing the following cost function:
 
-    f = g + h  where g are the number of moves and h is either the Manhattan or 
-    Hamming distance
+    f = g + h
 
-    '''
+    where g are the number of moves and h is either the Manhattan or
+    Hamming distances
+    """
 
-    def __init__(self, boardGame):
+    def __init__(self, boardGame: Board, heuristic: HeuristicDistance):
         self.board = boardGame
-        self.moves = 0             # counter to count the moves required to reach the solution
-        self.solutions = []        
-        self.__solve()             # remember the _ and __ expresses my intent of declare this 
-                                   # variables as private
-    def __solve(self): 
+        self.heuristic = heuristic
+        self.moves = 0  # counter to count the moves required to reach the solution
+        self.solutions: list[Board] = []
+        self.__solve()  # remember the _ and __ expresses my intent of declare this
+        # variables as private
+
+    def __solve(self):
         state = 0
-        search_node =  self.SearchNode(self.board,state,None)  # Create a search node (basically it's a linked list)
-        seeker_node = self.aStar(search_node)                  # I wrapped inmediately the initial search node
-                                                               # with the aStar class and its methods
-                                                               
-        
-        # Uncomment if you want to use my code for the min prority queue intead of the one
+        board_node = BoardNode(
+            self.board, state, None
+        )  # Create a node board (basically it's a linked list)
+        seeker_node = self.aStar(
+            board_node, self.heuristic
+        )  # I wrapped immediately the initial board node
+        # with the aStar class and its methods to perform calculations
+
+        # Uncomment if you want to use my code for the min probity queue intend of the one
         # provided by the heapq library
         # pq = minPQ.MinPQ()             # you can use this minPQ instead and compare which is faster
         # pq.insert(seeker_node)
 
         # Uncomment the two lines below to implement min heap queue provided by the heapq library
 
-        pq = []                  # the heapq library requires an empty list to build the heap  
-        heappush(pq,seeker_node) # and push the first element into this empty list using heappush 
+        pq = []  # the heapq library requires an empty list to build the heap
+        heappush(
+            pq, seeker_node
+        )  # and push the first element into this empty list using heappush
         i = 0
-        while(True): # the seeker node is a search node with the A* methods
+        while True:  # the seeker node is a search node with the A* methods
             # seeker_node = pq.delMin()   # uncomment if you are using the minPQ.py file
-            seeker_node = heappop(pq)     # uncomment if you are using the heapq library
-            search_node = seeker_node.get_Search_Node()
-            old_search_node = search_node       # We keep the previous move because inside this node there are 
-            state = old_search_node.state + 1   # an internal counter that help us to keep track the number  
-                                                # of moves for a particular node
-            if(search_node.link == None): predecessor = None  # if there isn't any link to another node, this node 
-            else: predecessor = search_node.link.current      # doesn't have a predecessor
-            # Uncomment for debbuging    
+            # get the board node processed by the A* algorithm
+            seeker_node = heappop(pq)  # uncomment if you are using the heapq library
+            board_node = (
+                seeker_node.get_Board_Node()
+            )  # get the board node with the lowest scoring function value
+            old_board_node = board_node  # We keep the previous move because inside this node there are
+            state = (
+                old_board_node.state + 1
+            )  # an internal counter that help us to keep track the number
+            # of moves for a particular node
+            if board_node.link == None:
+                predecessor = None  # if there isn't any link to another node, this node doesn't have a predecessor
+            else:
+                predecessor = (
+                    board_node.link.current
+                )  # else we set the current node as the predecessor for the next
+            # Uncomment for debbuging
             # print("Step: " + str(i))
             # print("Priority " + str(seeker_node.get_priority()))
-            # print("moves: " + str(search_node.state))
-            # print("manhattan " + str(search_node.current.manhattan()))
-            # print("search node: ") 
-            # print(search_node.current)
+            # print("moves: " + str(board_node.state))
+            # print("manhattan " + str(board_node.current.manhattan()))
+            # print("search node: ")
+            # print(board_node.current)
             # print('----------')
-            # print("neighbours")
+            # print("neighbors")
 
-            # Game tree loop. We itarete through the neighbors 
-            for neighbour in search_node.current.neighbors():
-                if(not (neighbour == predecessor)):  # to avoid repetitions the current neighbor must be different to the previous configuration
-                    # print(neighbour)
-                    search_node = self.SearchNode(neighbour,state,old_search_node)
-                    branch = self.aStar(search_node)
+            # Game tree loop. We iterate through the neighbors
+            for neighbor in board_node.current.neighbors():
+                if not (
+                    neighbor == predecessor
+                ):  # to avoid repetitions the current neighbor must be different to the previous configuration
+                    # print(neighbor)
+                    board_node = BoardNode(neighbor, state, old_board_node)
+                    branch = self.aStar(board_node, self.heuristic)
                     # pq.insert(branch)
-                    heappush(pq,branch)  
+                    heappush(pq, branch)
             i += 1
-            if(old_search_node.current.is_goal()): break
+            if old_board_node.current.is_goal():
+                break
         # uncomment to know how many iteretions were required to find the solution
         # print("iterations " + str(i))
 
-        # Once we found the goal board we used its link to retieve all the previous
+        # Once we found the goal board we used its link to retrieve all the previous
         # steps and we store them in a list
-        self.moves = state    # udate the number of moves   
-        search_node = search_node.link 
-        while(search_node != None):          
-            self.solutions.append(search_node.current)
-            search_node = search_node.link
+        self.moves = state  # update the number of moves
+        board_node = board_node.link
+        while board_node != None:
+            self.solutions.append(board_node.current)
+            board_node = board_node.link
 
-    # not all puzzles have solution, this method tell us before hand if a board is solvable        
+    # not all puzzles have solution, this method tell us before hand if a board is solvable
     def isSolvable(self):
+        """To be implemented"""
         pass
 
     def __iter__(self):
-        while(len(self.solutions) > 0):
+        """
+        Define a way to iterate on the solver
+        """
+        while len(self.solutions) > 0:
             yield self.solutions.pop()
 
     def number_of_moves(self):
         return self.moves - 1
 
-    class SearchNode(object):
-        '''
-        Recursive data structure, a wrapper for the class board to make a linked list
-        current: (Board) the current board instance in the solver 
-        state:   (int) the current state (move) in the solver
-        link:    (SearchNode) a link to the precedent SearchNode object
-        '''
-        def __init__(self, current, state, link):
-            self.current = current
-            self.state = state
-            self.link = link
-
     # @functools.total_ordering
     class aStar(object):
-        '''
+        """
         An extra layer for the board class. Here we are going to implement the
-        aStar algorithm to solve the 8-puzzle. The priority queue will mantain 
+        aStar algorithm to solve the 8-puzzle. The priority queue will mantain
         the priorities based on this computations
-        '''
-        def __init__(self, searchNode):
-            self.sn = searchNode
-            self.heuristic_distance = searchNode.current.manhattan() # can be replace with the hamming distance
-            self.state = searchNode.state
-            self.cost_function = self.heuristic_distance + searchNode.state
+        """
 
-        def get_Search_Node(self):
-            return self.sn
-        
+        def __init__(self, BoardNode: BoardNode, heuristic: HeuristicDistance):
+            self.BoardNode = BoardNode
+            self.heuristic_distance = self.getHeuristicDistance(
+                heuristic
+            )  # can be replace with the hamming distance
+            self.state = self.BoardNode.state
+            self.cost_function = self.heuristic_distance + self.BoardNode.state
+
+        def getHeuristicDistance(self, heuristic: HeuristicDistance):
+            match heuristic:
+                case heuristic.MANHATTAN:
+                    return self.BoardNode.current.manhattan()
+                case heuristic.HAMMING:
+                    return self.BoardNode.current.hamming()
+
+        def get_Board_Node(self):
+            return self.BoardNode
+
         def get_priority(self):
             return self.cost_function
 
         def __lt__(self, other):
-            return (self.cost_function, self.heuristic_distance) < (other.cost_function, other.heuristic_distance)
+            return (self.cost_function, self.heuristic_distance) < (
+                other.cost_function,
+                other.heuristic_distance,
+            )
 
         def __eq__(self, other):
-            return self.cost_function == other.cost_function 
+            return self.cost_function == other.cost_function
 
-# Uncomment to create the game board game from a random generator
-# n = 3   # size of the board
-# a = list(random.sample(range(n*n),n*n))  
-# blocks = [[a[n*i+j] for j in range(n)] for i in range(n) ]
 
-# Uncomment to create the board game reading from a file
-data_folder = Path("source_data/")
-file_to_open = data_folder / "puzzle04.txt"
-f = open(file_to_open)
-n = int(f.readline())
-blocks = [[ int(el) for el in line.split()] for line in f ]
+if __name__ == "__main__":
+    # Uncomment to create the game board game from a random generator
+    # n = 3   # size of the board
+    # a = list(random.sample(range(n*n),n*n))
+    # blocks = [[a[n*i+j] for j in range(n)] for i in range(n) ]
 
-board = Board(blocks)
-# print("initial board")
-# print(board)
-# print("------")
-solver = Solver(board)
+    # Uncomment to create the board game reading from a file
+    data_folder = Path("source_data/")
+    file_to_open = data_folder / "puzzle04.txt"
+    f = open(file_to_open)
+    z = int(f.readline())
+    blocks = [[int(el) for el in line.split()] for line in f]
 
-# Check if the board is solvable and if so, show how to solve it
-print("# of move to solve the board: "+  str(solver.number_of_moves()))
-for solutions in solver:
-    print(solutions)
+    board = Board(blocks)
+    # print("initial board")
+    # print(board)
+    # print("------")
+    solver = Solver(board, HeuristicDistance.MANHATTAN)
+
+    # Check if the board is solvable and if so, show how to solve it
+    print("# of move to solve the board: " + str(solver.number_of_moves()))
+    for solutions in solver:
+        print(solutions)
